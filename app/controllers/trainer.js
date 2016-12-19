062c4e0e02f6d7ee8207c4566af186eb7cee9a34
soeren van der wall <soeren.vanderwall@web.de>
2016-12-19 17:19:04 +0100

    namespaceerror fix in controllers/trainer.js

--- app/controllers/trainer.js
+++ app/controllers/trainer.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   catchError: function(error) {
     console.log(error.errors[0].detail);
-    _this.set('model.error', error.errors[0].detail);
+    this.set('model.error', error.errors[0].detail);
   },
   userStatement: Ember.computed(function() {
      if(this.get('model.userStatement')) {
@@ -15,7 +15,6 @@ export default Ember.Controller.extend({
     submitStatement(userStatement) {
       this.set('model.error', null);
       this.model.userStatement = userStatement;
-      let _this = this;
       this.model.save().catch(this.catchError);
     },
     finishTaskTrial() {
